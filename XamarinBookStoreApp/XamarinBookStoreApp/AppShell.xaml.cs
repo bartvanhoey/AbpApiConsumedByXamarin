cf4824e9dd18b7d3e4cefeec9c65e9eb5ccd38bc
bartvanhoey <bartvanhoey@hotmail.com>
2021-02-14 20:54:37 +0100

    IdentityConnectPage

--- XamarinBookStoreApp/XamarinBookStoreApp/AppShell.xaml.cs
+++ XamarinBookStoreApp/XamarinBookStoreApp/AppShell.xaml.cs
@@ -17,7 +17,7 @@ namespace XamarinBookStoreApp
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//LoginPage");
+            await Shell.Current.GoToAsync("//IdentityConnectPage");
         }
     }
 }
---
+++ XamarinBookStoreApp/XamarinBookStoreApp/Views/IdentityConnectPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinBookStoreApp.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinBookStoreApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IdentityConnectPage : ContentPage
+    {
+        public IdentityConnectPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new IdentityConnectViewModel();
+
+        }
+    }
+}
