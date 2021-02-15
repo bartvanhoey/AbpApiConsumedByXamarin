56281ae3cac54f300bb3f827678394369f803593
bartvanhoey <bartvanhoey@hotmail.com>
2021-02-15 20:06:15 +0100

    Update

--- XamarinBookStoreApp/XamarinBookStoreApp/App.xaml.cs
+++ XamarinBookStoreApp/XamarinBookStoreApp/App.xaml.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using XamarinBookStoreApp.Services;
-using XamarinBookStoreApp.Views;
 
 namespace XamarinBookStoreApp
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
